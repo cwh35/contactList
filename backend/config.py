2f297db6e36689b9034a4f4238ab3dcba45fa2d9
Cameron Henning <cwh35@pitt.edu>
2024-03-26 15:20:22 -0400

    configured config.py file to create instance of app and the database

--- backend/config.py
+++ backend/config.py
@@ -1,1 +1,18 @@
 # This will contain the main configuration for the application
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# CORS = Cross Origin Requests
+# This lets us send requests to the backend from a different URL
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///contactlistdatabase.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False # disables tracking all modifications made to the database
+
+# create instance of database
+# gives access to the database created above
+# allows us to use CRUD functions: create, read, update, and delete
+db = SQLAlchemy(app)
