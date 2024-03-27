8d0d9d9d7286283a9dcc7be226286b5e3b4c4259
Cameron Henning <cwh35@pitt.edu>
2024-03-27 13:03:40 -0400

    Added route for creating a contact

--- backend/main.py
+++ backend/main.py
@@ -10,6 +10,27 @@ def get_contacts():
 
     return jsonify({"contacts": json_contacts})
 
+@app.route("/create_contact", methods=["POST"])
+def create_contact():
+    first_name = request.json.get("firstName")
+    last_name = request.json.get("lastName")
+    email = request.json.get("email")
+    phone = request.json.get("phone")
+    address = request.json.get("address")
+
+    if not first_name or not last_name or not email:
+        return jsonify(({"message": "First name, last name, and email are required"}), 400)
+
+    # adding a new entry to the database
+    # create python class corresponding to that entry
+    new_contact = Contact(first_name=first_name, last_name=last_name, email=email, phone=phone, address=address)
+    try: 
+        db.session.add(new_contact) # add to database session
+        db.session.commit() # commit the session
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
+    
+    return jsonify({"message": "Contact created successfully!"}), 201
 
 if __name__ == "__main__":
     with app.app_context():
